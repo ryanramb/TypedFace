515a6735af4ecdcdb4db3231a150450647b60d26
ryanramb <ryanrambaran.fl@gmail.com>
2022-03-28 20:40:22 -0400

    mobile v1

--- src/App.js
+++ src/App.js
@@ -60,81 +60,83 @@ function App() {
   return (
     <div className='App'>
       <section className='options'>
-        <h1>TypedFaces</h1>
+        <div className='primary'>
+          <h1>TypedFaces</h1>
+          <div className='option'>
+            <label>Your text</label>
+            <input
+              type='text'
+              name='sentence'
+              placeholder='The quick brown fox...'
+              onKeyUp={updateOuput}
+            ></input>
+          </div>
 
-        <div className='option'>
-          <label>Your text</label>
-          <input
-            type='text'
-            name='sentence'
-            placeholder='The quick brown fox...'
-            onKeyUp={updateOuput}
-          ></input>
+          <div className='option'>
+            <label>Type Face</label>
+            <select name='typeface' onChange={updateFamily}>
+              <option value='Kanit'>Kanit</option>
+              <option value='Montserrat'>Montserrat</option>
+              <option value='Poppins'>Poppins</option>
+              <option value='Taviraj'>Taviraj</option>
+              <option value='Tourney'>Tourney</option>
+              <option value='Urbanist'>Urbanist</option>
+            </select>
+          </div>
         </div>
+        <div className='secondary'>
+          <div className='option'>
+            <label>
+              Size<span className='text-Output'>{fontSize}px</span>
+            </label>
+            <input
+              type='range'
+              name='typesize'
+              min={12}
+              max={200}
+              defaultValue={fontSize}
+              onChange={updateFont}
+            />
+          </div>
 
-        <div className='option'>
-          <label>Type Face</label>
-          <select name='typeface' onChange={updateFamily}>
-            <option value='Kanit'>Kanit</option>
-            <option value='Montserrat'>Montserrat</option>
-            <option value='Poppins'>Poppins</option>
-            <option value='Taviraj'>Taviraj</option>
-            <option value='Tourney'>Tourney</option>
-            <option value='Urbanist'>Urbanist</option>
-          </select>
-        </div>
+          <div className='option'>
+            <label>
+              Weight <span className='weight-Output'>{weight}</span>
+            </label>
+            <input
+              type='range'
+              name='typeweight'
+              min={100}
+              max={900}
+              step={100}
+              defaultValue={weight}
+              onChange={updateWeight}
+            />
+          </div>
 
-        <div className='option'>
-          <label>
-            Type Size <span className='text-Output'>{fontSize}px</span>
-          </label>
-          <input
-            type='range'
-            name='typesize'
-            min={12}
-            max={200}
-            defaultValue={fontSize}
-            onChange={updateFont}
-          />
-        </div>
+          <div className='option'>
+            <label>
+              Leading <span className='lineheight-Output'>{leading}</span>
+            </label>
+            <input
+              type='range'
+              name='leading'
+              min={0.7}
+              max={2.5}
+              step={0.1}
+              defaultValue={leading}
+              onChange={updateLeading}
+            />
+          </div>
 
-        <div className='option'>
-          <label>
-            Font Weight <span className='weight-Output'>{weight}</span>
-          </label>
-          <input
-            type='range'
-            name='typeweight'
-            min={100}
-            max={900}
-            step={100}
-            defaultValue={weight}
-            onChange={updateWeight}
-          />
-        </div>
-
-        <div className='option'>
-          <label>
-            Leading <span className='lineheight-Output'>{leading}</span>
-          </label>
-          <input
-            type='range'
-            name='leading'
-            min={0.7}
-            max={2.5}
-            step={0.1}
-            defaultValue={leading}
-            onChange={updateLeading}
-          />
-        </div>
-
-        <div className='option option-checkbox'>
-          <label>Italic</label>
-          <input
-            type='checkbox'
-            name='italic'
-            onChange={updateFontStyle}
-          ></input>
+          <div className='option option-checkbox'>
+            <label>Italic</label>
+            <input
+              type='checkbox'
+              name='italic'
+              onChange={updateFontStyle}
+            ></input>
+          </div>
         </div>
 
         {/* <div className='option'>
